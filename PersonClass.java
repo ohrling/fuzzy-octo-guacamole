3cff27f47c2607fea0ea33b056c841138b46947a
alfred grimlund <alfred@localhost.localdomain>
2019-01-10 17:44:22 +0100

    lade till en klass som beskriver en person till register

---
+++ PersonClass.java
@@ -0,0 +1,19 @@
+class PersonClass{
+
+private String  PersonName;
+private int PersonAge; 
+
+
+public PersonClass(String name, int age){
+PersonName=name;
+PersonAge=age; 
+
+	}
+
+public String toString(){
+return "persons name is " + PersonName + "and is " + PersonAge + "years old"; 
+
+	}
+
+}
+
