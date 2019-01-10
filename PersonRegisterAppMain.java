ad77b84849eeca86b3868aa26ceadc8648a27409
alfred grimlund <alfred@localhost.localdomain>
2019-01-10 17:54:19 +0100

    lade till en ny mainklass

---
+++ PersonRegisterAppMain.java
@@ -0,0 +1,12 @@
+class PersonRegisterAppMain{
+
+
+PersonClass kalle = new PersonClass("Kalle kalleson", 64);
+
+PersonClass berit  = new PersonClass("Berit Beritsson", 24);
+
+System.out.println(kalle.toString());
+System.out.println(berit.toString());
+
+
+}
