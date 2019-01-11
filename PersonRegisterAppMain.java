f583a66a2ceafcc79223069c065dea68f6ca55cd
alfred <alfred.grimlund@gmail.com>
2019-01-11 04:09:19 +0100

    lade till en mainmetod

--- PersonRegisterAppMain.java
+++ PersonRegisterAppMain.java
@@ -1,12 +1,12 @@
 class PersonRegisterAppMain{
 
-
+public static void main(String[]args){
 PersonClass kalle = new PersonClass("Kalle kalleson", 64);
 
 PersonClass berit  = new PersonClass("Berit Beritsson", 24);
 
 System.out.println(kalle.toString());
 System.out.println(berit.toString());
-
+}
 
 }
